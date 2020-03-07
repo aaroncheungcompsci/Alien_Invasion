f0101c0b1cc7a31ffad7705e6c3398b54a02263c
Aaron Cheung <aaroncheungcompsci@gmail.com>
2020-03-07 00:33:39 -0800

    Added Ship class and refactored code

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,6 +1,40 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
+
+
+def _check_keydown_events(self, event):
+    if event.key == pygame.K_RIGHT:
+        self.ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        self.ship.moving_left = True
+    elif event.key == pygame.K_q:
+        sys.exit()
+
+
+def _check_keyup_events(self, event):
+    if event.key == pygame.K_RIGHT:
+        self.ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        self.ship.moving_left = False
+
+
+# _ before name means "private" or internal use in python
+def _check_events(self):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            _check_keydown_events(self, event)
+        elif event.type == pygame.KEYUP:
+            _check_keyup_events(self, event)
+
+
+def _update_screen(self):
+    self.screen.fill(self.bg_color)
+    self.ship.blitme()
+    pygame.display.flip()
 
 
 class AlienInvasion:
@@ -9,11 +43,11 @@ class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
-
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
 
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
         self.bg_color = (230, 230, 230)
 
     def run_game(self):
@@ -21,14 +55,10 @@ class AlienInvasion:
 
         while True:
             # watch for events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
+            _check_events(self)
+            self.ship.update()
             # redraw screen on each iteration of while loop
-            self.screen.fill(self.bg_color)
-
-            pygame.display.flip()
+            _update_screen(self)
 
 
 if __name__ == '__main__':
---
+++ alien_invasion/ship.py
@@ -0,0 +1,33 @@
+import pygame
+
+
+class Ship:
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # start at bottom center
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        self.x = float(self.rect.x)
+
+        self.moving_right = False  # movement flags
+        self.moving_left = False
+
+    def blitme(self):
+        """draw ship"""
+        self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        """update based on movement flag"""
+        # also prevents going off the screen
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        elif self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
