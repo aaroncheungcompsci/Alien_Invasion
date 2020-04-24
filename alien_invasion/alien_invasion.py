092b8779a287945a60c8326e7001c34fce3a5920
Aaron Cheung <aaroncheungcompsci@gmail.com>
2020-04-24 10:37:44 -0700

    created full fleet of aliens

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -41,23 +41,34 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
-    def _create_alien(self, alien_number):
+    def _create_alien(self, alien_number, row_number):
         # create alien and place in row
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
     def _create_fleet(self):
         """create fleet of aliens"""
         # create first row of aliens
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
-        for alien_number in range(number_aliens_x):
-            self._create_alien(alien_number)
+
+        # determine number of rows of aliens that fit on screen
+        ship_height = self.ship.rect.height
+
+        # this is to create space above the ship
+        available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+
+        # create the full fleet
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
 
     # _ before name means "private" or internal use in python
     def _check_events(self):
